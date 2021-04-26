6c614522753de9b0c1d659dec190071769645edb
yunyan2020 <li_yunyan@hotmail.com>
2021-04-26 13:30:36 +0200

    Update store.js
    In order to test commit

---
+++ client/src/store.js
@@ -0,0 +1,16 @@
+import {createStore} from 'vuex'
+
+export default createStroe({
+  state: {
+    match: {
+      id: 1,
+      homeTeam: "Lugi",
+      awayTeam: "H43",
+      homeTeamScore: 33,
+      awayTeamScore: 13,
+      matchEvents: [
+      
+      ]
+    }
+  }
+})
