36f7734276c7ade270d3b67be3be1af10f43787a
Kristian <the.dyze@gmail.com>
2021-04-28 10:19:49 +0200

    timer working

--- client/src/components/PeriodTimer.vue
+++ client/src/components/PeriodTimer.vue
@@ -1,26 +1,16 @@
 <template>
   <div class="timer-container">
-    <!-- <div class="time">
-    <h1>{{ time }}</h1>
-  </div> -->
+
     <div class="timer">
       <span id="minutes">{{ minutes }}</span>
       <span id="middle">:</span>
       <span id="seconds">{{ seconds }}</span>
     </div>
+
     <div class="buttons">
-      <!--     Start TImer -->
       <button id="start" class="start" v-if="!timer" @click="startTimer">Start</button>
-      <!--     Pause Timer -->
       <button id="pause" class="pause" v-if="timer" @click="stopTimer">Pause</button>
-      <!--     Restart Timer -->
-      <button id="reset"
-        class="reset"
-        v-if="resetButton"
-        @click="resetTimer"
-      >
-        Reset
-      </button>
+      <button id="reset" class="reset" v-if="resetButton" @click="resetTimer">Reset</button>
     </div>
   </div>
 </template>
@@ -31,9 +21,7 @@ export default {
     return {
       timer: null,
       totalTime: 58,
-      // totalTime: 25 * 60,
       resetButton: false,
-      title: "Let the countdown begin!!",
     };
   },
   computed: {
@@ -53,30 +41,29 @@ export default {
     startTimer: function () {
       this.timer = setInterval(() => this.countdown(), 1000);
       this.resetButton = true;
-      this.title = "Greatness is within sight!!";
+      
     },
     stopTimer: function () {
       clearInterval(this.timer);
       this.timer = null;
       this.resetButton = true;
-      this.title = "Never quit, keep going!!";
+      
     },
     resetTimer: function () {
       this.totalTime = 0;
       clearInterval(this.timer);
       this.timer = null;
       this.resetButton = false;
-      this.title = "Let the countdown begin!!";
+      
     },
     padTime: function (time) {
       return (time < 10 ? "0" : "") + time;
     },
     countdown: function () {
-      if (this.totalTime <= 30* 60) {
+      if (this.totalTime < 1 * 60) {
         this.totalTime++;
       } else {
-        this.totalTime = 30;
-        this.resetTimer();
+        this.stopTimer();
       }
     },
   },
